7ad17635b2b1e1ba62ab881d0d164f2f31e40bd6
Bogdan <c0pland@icloud.com>
2022-09-14 21:00:50 +0300

    add switcher between grid and list views

--- Animafrica/App/ContentView.swift
+++ Animafrica/App/ContentView.swift
@@ -9,20 +9,65 @@ import SwiftUI
 
 struct ContentView: View {
 	let animals: [Animal] = Bundle.main.decode("animals.json")
+	let haptics = UIImpactFeedbackGenerator(style: .medium)
+	let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 2)
+	@State private var isGridViewActive = false
+	
     var body: some View {
-		NavigationView{
-			List{
-				CoverImageView()
-					.frame(height: 300)
-					.listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-				ForEach(animals) { animal in
-					NavigationLink(destination: AnimalDetailView(animal: animal)) {
-						AnimalListItemView(animal: animal)
+		NavigationView {
+			Group {
+				if !isGridViewActive {
+					List {
+						CoverImageView()
+							.frame(height: 300)
+							.listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+						ForEach(animals) { animal in
+							NavigationLink(destination: AnimalDetailView(animal: animal)) {
+								AnimalListItemView(animal: animal)
+							}
+						}
+					}
+				} else {
+					ScrollView(.vertical, showsIndicators: false) {
+						LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+							ForEach(animals) { animal in
+								NavigationLink(destination: AnimalDetailView(animal: animal)) {
+									AnimalGridItemView(animal: animal)
+								}
+							}
+						}
+						.padding(10)
+						.animation(.easeIn)
 					}
 				}
 			}
 			.navigationTitle("Africa")
 			.navigationBarTitleDisplayMode(.large)
+			.toolbar {
+				ToolbarItem(placement: .navigationBarTrailing) {
+					HStack(spacing: 16) {
+						Button {
+							print("List")
+								isGridViewActive = false
+							haptics.impactOccurred()
+						} label: {
+							Image(systemName: "square.fill.text.grid.1x2")
+								.font(.title3)
+								.foregroundColor(isGridViewActive ? .primary : .accentColor)
+						}
+						Button {
+							print("Grid")
+							isGridViewActive = true
+							haptics.impactOccurred()
+						} label: {
+							Image(systemName: "square.grid.2x2")
+								.font(.title3)
+								.foregroundColor(isGridViewActive ? .accentColor : .primary)
+						}
+
+					}
+				}
+			}
 		}
     }
 }
